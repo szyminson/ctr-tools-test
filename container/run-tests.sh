bb0a954f19877472bcea2a8c60ee5e34a62194a9
szyminson <szyminson@hotmail.com>
2023-05-28 15:42:34 +0200

    refactor: move container files to dedicated subdir

--- container/run-tests.sh
+++ container/run-tests.sh
@@ -4,5 +4,6 @@ PTS=/phoronix-test-suite/phoronix-test-suite
 $PTS batch-run pts/sysbench || exit 1
 LATEST_RESULT=$(ls -l /var/lib/phoronix-test-suite/test-results | tail -1 | awk '{print $NF}')
 $PTS result-file-to-json $LATEST_RESULT
-cp "/root/$LATEST_RESULT.json" /root/latest.json
-cat /root/latest.json
+cp "/root/$LATEST_RESULT.json" /var/ctr-tools-test/results
+cp "/root/$LATEST_RESULT.json" /var/ctr-tools-test/results/latest.json
+cat /var/ctr-tools-test/results/latest.json
