c712823cec43d4a675a25a9946c5dd67c848c46a
szyminson <szyminson@hotmail.com>
2023-05-29 14:53:42 +0200

    fix: run test suite instead of test

--- container/run-tests.sh
+++ container/run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PTS=/phoronix-test-suite/phoronix-test-suite
 
-$PTS batch-run pts/sysbench || exit 1
+$PTS batch-run ctr-tools-test || exit 1
 LATEST_RESULT=$(ls -l /var/lib/phoronix-test-suite/test-results | tail -1 | awk '{print $NF}')
 $PTS result-file-to-json $LATEST_RESULT
 cp "/root/$LATEST_RESULT.json" /var/ctr-tools-test/results
