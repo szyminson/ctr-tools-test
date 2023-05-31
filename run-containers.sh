1ef855592900190dbd10c9c8160f9ced04662d39
Szymon Porzezinski <s.porzezinski@bonasoft.pl>
2023-05-31 23:28:20 +0200

    feat: tmux with logging and separate tasks

---
+++ run-containers.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Exit whole script on SIGINT
+
+# Arguments and defaults
+TESTS_CONTAINER=${1:-"ghcr.io/szyminson/ctr-tools-test:latest"}
+CONTAINER_NAME=${2:-"ctr-tools-test"}
+IN_CONTAINER_RESULT_DIR=${3:-"/var/ctr-tools-test/results"}
+RESULT_BASE_DIR=${4:-"/tmp"}
+SIG_HANDLER="docker rm $CONTAINER_NAME; podman rm $CONTAINER_NAME; exit"
+
+# Setup tmux logging if in a tmux session
+if [ "$TERM" = "screen" ] && [ -n "$TMUX" ]; then
+    TMUX_LOG_FILE="$RESULT_BASE_DIR/tmux.log"
+    # Move previous log to different file if exists
+    [ -f $TMUX_LOG_FILE ] && mv $TMUX_LOG_FILE "$TMUX_LOG_FILE.$(date +%s)"
+    tmux pipe-pane -o "cat >> $TMUX_LOG_FILE"
+fi
+
+echo "Removing containers if exist"
+docker rm $CONTAINER_NAME
+podman rm $CONTAINER_NAME
+
+echo "Running Docker test"
+docker run --rm --name $CONTAINER_NAME -v "$RESULT_BASE_DIR/docker:$IN_CONTAINER_RESULT_DIR" \
+    $TESTS_CONTAINER
+echo "Running podman test"
+podman run --rm --name $CONTAINER_NAME -v "$RESULT_BASE_DIR/podman:$IN_CONTAINER_RESULT_DIR" \
+    $TESTS_CONTAINER
